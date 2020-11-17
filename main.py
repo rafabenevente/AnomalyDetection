8b4d76a32ab46f5a4525fe6c42402143e595b737
Rafael <rafabenevente@gmail.com>
2020-11-16 21:34:23 -0300

    Correções na rotina.

--- main.py
+++ main.py
@@ -5,20 +5,11 @@ from alibi_detect.utils.visualize import plot_instance_score
 from data_loader import DataLoader
 from model import Model
 
-is_kaggle = True if 'rafael' in os.getcwd() else False
+is_kaggle = False if 'rafael' in os.getcwd() else True
 
 size = (64, 64)
 
 if is_kaggle:
-    path_train = "/home/rafael/repo/AnomalyDetection/Data/stage_2_train_images"
-    path_test = "/home/rafael/repo/AnomalyDetection/Data/stage_2_test_images"
-
-    train_df = pd.read_csv("/home/rafael/repo/AnomalyDetection/Data/train.csv")
-    path_train_sub = "/home/rafael/repo/AnomalyDetection/Data/images"
-
-    sub_df = pd.read_csv("/home/rafael/repo/AnomalyDetection/Data/sample_submission.csv")
-    path_test_sub = "/home/rafael/repo/AnomalyDetection/Data/images"
-else:
     path_train = "../input/rsna-pneumonia-detection-challenge/stage_2_train_images"
     path_test = "../input/rsna-pneumonia-detection-challenge/stage_2_test_images"
 
@@ -27,6 +18,15 @@ else:
 
     sub_df = pd.read_csv("../input/chest-xray-anomaly-detection/sample_submission.csv")
     path_test_sub = "../input/chest-xray-anomaly-detection/images"
+else:
+    path_train = "/home/rafael/repo/AnomalyDetection/Data/stage_2_train_images"
+    path_test = "/home/rafael/repo/AnomalyDetection/Data/stage_2_test_images"
+
+    train_df = pd.read_csv("/home/rafael/repo/AnomalyDetection/Data/train.csv")
+    path_train_sub = "/home/rafael/repo/AnomalyDetection/Data/images"
+
+    sub_df = pd.read_csv("/home/rafael/repo/AnomalyDetection/Data/sample_submission.csv")
+    path_test_sub = "/home/rafael/repo/AnomalyDetection/Data/images"
 
 print("train")
 train, train_view_pos = DataLoader.img_to_np(path_train, size)
@@ -52,22 +52,22 @@ labels = ['normal', 'outlier']
 # PA ON PA
 print("PA ON PA")
 pred_pa_pa = model_pa.predict(test_pa)
-# plot_instance_score(pred_pa_pa, target_pa, labels, model_pa.get_threshold())
+plot_instance_score(pred_pa_pa, target_pa, labels, model_pa.get_threshold())
 
 # PA ON AP
 print("PA ON AP")
 pred_pa_ap = model_pa.predict(test_ap)
-# plot_instance_score(pred_pa_ap, target_ap, labels, model_pa.get_threshold())
+plot_instance_score(pred_pa_ap, target_ap, labels, model_pa.get_threshold())
 
 # AP ON AP
 print("AP ON AP")
 pred_ap_ap = model_ap.predict(test_ap)
-# plot_instance_score(pred_ap_ap, target_ap, labels, model_ap.get_threshold())
+plot_instance_score(pred_ap_ap, target_ap, labels, model_ap.get_threshold())
 
 # AP ON PA
 print("AP ON PA")
 pred_ap_pa = model_ap.predict(test_pa)
-# plot_instance_score(pred_ap_pa, target_pa, labels, model_ap.get_threshold())
+plot_instance_score(pred_ap_pa, target_pa, labels, model_ap.get_threshold())
 
 
 # SUB
